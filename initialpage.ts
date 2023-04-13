555451dee0bf03e1cd1d459a1b679f89e0f795bc
Arnau Merino <arnaumevi@gmail.com>
2023-04-13 21:33:36 +0200

    Possible solution

--- initialpage.ts
+++ initialpage.ts
@@ -17,15 +17,15 @@ export async function createHeader(): Promise<string> {
           flex-wrap: wrap;
           justify-content: center;
           align-items: center;
-          background-color: #f7f7f7;
+          background-color: #bcf8ff;
         }
         .card {
           width: 250px;
           margin: 1rem;
-          background-color: white;
-          border: 1px solid #ccc;
-          border-radius: 5px;
-          box-shadow: 0 0 5px #ccc;
+          background-color: rgb(43, 250, 24);
+          border: 10px solid #000000;
+          border-radius: 15px;
+          box-shadow: 0 0 5px #0e8526;
           text-align: center;
           padding: 1rem;
         }
@@ -69,8 +69,59 @@ export async function getCharacterCardsHTML(): Promise<string> {
         <div class="card">
           <img src="${character.image}" alt="${character.name}" />
           <h2>${character.name}</h2>
-          <p>ID: ${character.id}</p>
+            <p>ID: ${character.id}</p>
+            <p>GENDER: ${character.gender}</p>
+            <p>ORIGIN: ${character.origin}</p>
+            <p>STATUS: ${character.status}</p>
+            <p>SPECIE: ${character.species}</p>
+            <p>TYPE: ${character.type}</p>
+
         </div>
+        <style>
+        body {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+          align-items: center;
+          background-color: #f7f7f7;
+        }
+        .card {
+          width: 550px;
+          margin: 1rem;
+          background-color: rgb(43, 250, 24);
+          border: 10px solid #000000;
+          border-radius: 5px;
+          box-shadow: 0 0 15px #0e8526;
+          text-align: center;
+          padding: 1rem;
+        }
+        .card img {
+          width: 450px;
+          height: 450px;
+          object-fit: cover;
+          border-radius: 50%;
+          margin: 0 auto;
+          display: block;
+        }
+        .card h2 {
+          margin-top: 0.5rem;
+        }
+        .card p {
+          margin: 0.5rem 0;
+        }
+        .card a {
+          display: inline-block;
+          padding: 0.5rem;
+          background-color: #0f7ae5;
+          color: white;
+          text-decoration: none;
+          border-radius: 5px;
+          margin-top: 1rem;
+        }
+        .card a:hover {
+          background-color: #0055a5;
+        }
+      </style>
       `;
     fs.writeFile("./characters/" + character.id + ".html", htmlContent, () => {
       /* handle error */
@@ -80,7 +131,7 @@ export async function getCharacterCardsHTML(): Promise<string> {
         <div class="card" onclick="window.location.href='./characters/${character.id}.html';">
           <img src="${character.image}" alt="${character.name}" />
           <h2>${character.name}</h2>
-          <p>ID: ${character.id}</p>
+        
         </div>
       `;
   }
